b2de2fb89939b91af003f5257cf097d3d269096d
AffinityUniverse <166306397+AffinityUniverse@users.noreply.github.com>
2026-07-20 10:56:10 +0900

    Update flashcard-decks.js

--- flashcard-decks.js
+++ flashcard-decks.js
@@ -766,8 +766,9 @@
                     [
                         "The circle is more vivid than the square.",
                         "원이 사각형보다 더 선명합니다.",
+                        [
                         "circle",
-                        "square"
+                        "square"]
                     ]
                 ]
             )
